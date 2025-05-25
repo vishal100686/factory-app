4d583e44a77259a28fdb35c4d8dabbc18dda877b
vishal100686 <bhilai100686@gmail.com>
2025-05-25 07:54:15 +0530

    Initial commit with project files

--- index.tsx
+++ index.tsx
@@ -1,26 +1,10 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-<<<<<<< HEAD
-=======
-import { AppProvider } from './contexts/AppContext';
->>>>>>> 88260e65f84fd5b0bc475028cd30d2c3bc2c24af
+import './index.css'; // Agar aap CSS use kar rahe ho
 
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
-
-const root = ReactDOM.createRoot(rootElement);
-root.render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-<<<<<<< HEAD
     <App />
-=======
-    <AppProvider>
-      <App />
-    </AppProvider>
->>>>>>> 88260e65f84fd5b0bc475028cd30d2c3bc2c24af
   </React.StrictMode>
 );
